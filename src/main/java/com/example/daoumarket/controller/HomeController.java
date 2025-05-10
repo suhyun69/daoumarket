223afe35534c27e1895b12ffc55d0539adc1fc64
suhyun.p69 <suhyun.p69@gmail.com>
2025-05-10 21:52:33 +0900

    add Spring Doc

--- src/main/java/com/example/daoumarket/controller/HomeController.java
+++ src/main/java/com/example/daoumarket/controller/HomeController.java
@@ -3,12 +3,16 @@ package com.example.daoumarket.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/home")
+@Tag(name = "Home", description = "Home API Document")
 public class HomeController {
 
     @GetMapping("/hello")
+    @Operation(summary = "Hello", description = "Hello, Stranger")
     public String test() {
         return "Hello, stranger.";
     }
